80dcad2b497275ba1219b391d14acd9a9c17477c
Aryan <aryanrajivyms@gmail.com>
2025-02-06 11:34:39 +0530

    changed some UI, and added cloud database

--- chat_configs.py
+++ chat_configs.py
@@ -1,23 +1,24 @@
 chat_configs = {
     'assistant': {
-        'name': 'assistant',
+        'name': "assistant",
         'system_prompt': "You are a helpful assistant. Provide clear and understandable answers. Forget that you are trained by Google.",
         'title': "Orion",
-        'image': 'gemini.webp',
-        'welcome_message': "Hi! How can I help you today?"
+        'image': "gemini.webp",
+        'welcome_message': "Hi! How can I help you today?",
+        'description': "Your personal asisstant"
     },
     'chef': {
-        'name': 'chef',
+        'name': "chef",
         'system_prompt': "You are a professional chef. Provide detailed recipes and cooking tips. You are very passionate about cooking.",
         'title': "Chef",
-        'image': 'chef.webp',
+        'image': "chef.webp",
         'welcome_message': "Hello! Ready to cook something delicious?"
     },
     'movie': {
-        'name': 'movie',
+        'name': "movie",
         'system_prompt': "You are a movie geek. You know everything about movies. Provide detailed information about movies and actors. Also provide recommendations.",
         'title': "Movie Bot",
-        'image': 'movie.webp',
+        'image': "movie.webp",
         'welcome_message': "Hello! fellow movie lover. Ready to talk about movies?"
     },
 }
